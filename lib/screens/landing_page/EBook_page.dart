a6551785b3f05a3fee17c2bacd298f6b3f4b9fa1
nadamahbubatun <nadamahbubatun@gmail.com>
2024-11-04 13:57:30 -0800

    landing_ebook

--- lib/screens/landing_page/EBook_page.dart
+++ lib/screens/landing_page/EBook_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:project_tc/components/animation/animation_function.dart';
 import 'package:project_tc/components/constants.dart';
@@ -36,109 +37,124 @@ class _EbookPageState extends State<EbookPage> {
           ],
         ),
         Padding(
-          padding: EdgeInsets.only(
-            top: getValueForScreenType<double>(
-              context: context,
-              mobile: 10,
-              tablet: 50,
-              desktop: 100,
-            ),
+        padding: EdgeInsets.only(
+          top: getValueForScreenType<double>(
+            context: context,
+            mobile: 40,
+            tablet: 70,
+            desktop: 100,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StreamBuilder(
-                stream: FirestoreService.withoutUID().allEbook,
-                builder: (BuildContext context, snapshot) {
-                  if (snapshot.hasData) {
-                    final List<Map> dataMaps = snapshot.data!;
-
-                    final List<Map> ebook = dataMaps
-                        .where((ebookData) {
-                          final dynamic data = ebookData['ebook'];
-                          return data is EbookModel;
-                        })
-                        .where(
-                            (ebookData) => ebookData['ebook'].isDraft == false)
-                        .where((ebookData) =>
-                            ebookData['ebook'].title.toLowerCase())
-                        .map((ebookData) {
-                          final EbookModel ebook = ebookData['ebook'];
-                          final String id = ebookData['id'];
-                          return {'ebook': ebook, 'id': id};
-                        })
-                        .toList();
-
-                    // final List<Map> ebook = dataMaps.where((ebookMap) {
-                    //   final dynamic data = ebookMap['ebook'];
-                    //   return data is EbookModel?;
-                    // }).map((ebookMap) {
-                    //   final EbookModel ebook = ebookMap['ebook'];
-                    //   final String id = ebookMap['id'];
-                    //   return {'ebook': ebook, 'id': id};
-                    // }).toList();
-
-                    return SizedBox(
-                        width: getValueForScreenType<double>(
-                          context: context,
-                          mobile: width / 1.3,
-                          tablet: width / 1.6,
-                          desktop: width / 1.6,
-                        ),
-                        height: getValueForScreenType<double>(
-                          context: context,
-                          mobile: height * .64 * ebook.length,
-                          tablet: height * .64 * ebook.length + 150,
-                          desktop: height * .64 * ebook.length + 150,
-                        ),
-                        child: ebook.isNotEmpty
-                            ? LiveList(
-                                showItemInterval:
-                                    const Duration(milliseconds: 150),
-                                showItemDuration:
-                                    const Duration(milliseconds: 350),
-                                scrollDirection: Axis.vertical,
-                                itemCount: ebook.length,
-                                itemBuilder: animationBuilder(
-                                  (index) => EbookLists(
-                                      ebook: ebook[index]['article'],
-                                      id: ebook[index]['id']),
-                                ),
-                              )
-                            : Center(
-                                child: Text(
-                                  'Not Found',
-                                  style: GoogleFonts.mulish(
-                                    color: CusColors.secondaryText,
-                                    fontSize: getValueForScreenType<double>(
-                                      context: context,
-                                      mobile: width * .024,
-                                      tablet: width * .014,
-                                      desktop: width * .011,
-                                    ),
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ));
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(
-                      child: Text('No video learning available.'),
-                    );
-                  } else {
-                    return const Center(
-                      child: Text('kok iso.'),
-                    );
-                  }
-                },
-              )
-            ],
+          bottom: getValueForScreenType<double>(
+            context: context,
+            mobile: 40,
+            tablet: 70,
+            desktop: 100,
           ),
-        )
+        ),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          StreamBuilder(
+              stream: FirestoreService.withoutUID().allEbook,
+              builder: (BuildContext context, snapshot) {
+                if (snapshot.hasData) {
+                  final List<Map> dataMaps = snapshot.data!;
+                  final List<Map> Ebook = dataMaps.where((ebookMap) {
+                    final dynamic data = ebookMap['ebook'];
+                    return data is EbookModel?;
+                     
+                  }).map((ebookMap) {
+                    final EbookModel ebooks = ebookMap['ebook'];
+                    final String id = ebookMap['id'];
+                    return {'ebook': ebooks, 'id': id};
+                  }).toList();
+                  return Column(children: [
+                    Text(
+                      'Available E-book',
+                      style: GoogleFonts.mulish(
+                          color: CusColors.header,
+                          fontSize: getValueForScreenType<double>(
+                            context: context,
+                            mobile: width * .028,
+                            tablet: width * .022,
+                            desktop: width * .018,
+                          ),
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(
+                            top: getValueForScreenType<double>(
+                              context: context,
+                              mobile: 10,
+                              tablet: 20,
+                              desktop: 26,
+                            ),
+                            bottom: getValueForScreenType<double>(
+                              context: context,
+                              mobile: 40,
+                              tablet: 50,
+                              desktop: 70,
+                            )),
+                        width: width * .05,
+                        height: 2,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: const Color.fromRGBO(0, 0, 0, 1),
+                        )),
+                    SizedBox(
+                      height: getValueForScreenType<double>(
+                        context: context,
+                        mobile: height / 3.3 * Ebook.length,
+                        tablet: (height / 2.8) * (Ebook.length / 2),
+                        desktop: (height / 2) * (Ebook.length / 3),
+                      ),
+                      width: getValueForScreenType<double>(
+                        context: context,
+                        mobile: width / 2,
+                        tablet: width / 1.8,
+                        desktop: width / 1.7,
+                      ),
+                      child: ScrollConfiguration(
+                        behavior: ScrollConfiguration.of(context)
+                            .copyWith(scrollbars: false),
+                        child: MasonryGridView.count(
+                          physics: const ScrollPhysics(
+                              parent: BouncingScrollPhysics()),
+                          crossAxisSpacing: width *
+                              .02, // Adjust spacing between items horizontally
+                          mainAxisSpacing:
+                              16.0, // Adjust spacing between rows vertically
+                          crossAxisCount: getValueForScreenType<int>(
+                            context: context,
+                            mobile: 1,
+                            tablet: 2,
+                            desktop: 3,
+                          ),
+                          itemCount: Ebook.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Ebooks(
+                                ebook: Ebook[index]['ebook'],
+                                id: Ebook[index]['id']);
+                          },
+                        ),
+                      ),
+                    ),
+                  ]);
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No ebook available.'),
+                  );
+                } else {
+                  return const Center(
+                    child: Text('kok iso.'),
+                  );
+                }
+              })
+        ]),
+      ),
       ],
     );
   }
